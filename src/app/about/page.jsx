62d5735b5e22bf4dfafeb3bf03ca6a7a2d87db69
Vishwas M S <scorepulse.dev@gmail.com>
2024-08-26 23:01:58 +0530

    add about page

---
+++ src/app/about/page.jsx
@@ -0,0 +1,19 @@
+import React from "react";
+
+export default function About() {
+  return (
+    <div className="max-w-6xl mx-auto space-y-4 p-4">
+      <h1>About</h1>
+      <p>
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. In nam, sed,
+        provident quae deleniti quasi eum ratione ad sit voluptas sequi
+        similique, illo autem quisquam suscipit iste maiores unde itaque.
+      </p>
+      <p>
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. In nam, sed,
+        provident quae deleniti quasi eum ratione ad sit voluptas sequi
+        similique, illo autem quisquam suscipit iste maiores unde itaque.
+      </p>
+    </div>
+  );
+}
